80d1517f51b4294b7d131842e341be3f281f8a90
local <xingyi0201@gmail.com>
2021-10-24 03:15:01 +0300

    fix: Removed Pods

--- Pods/Target Support Files/KinBase/KinBase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KinBase : NSObject
-@end
-@implementation PodsDummy_KinBase
-@end
--- Pods/Target Support Files/KinSodium/KinSodium-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KinSodium : NSObject
-@end
-@implementation PodsDummy_KinSodium
-@end
